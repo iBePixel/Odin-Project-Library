6b91884d81d4757923c101f23cde3715b1ee536b
Elliott Lamb <53214529+iBePixel@users.noreply.github.com>
2025-02-09 14:03:48 +0000

    Add book function and stored object in array

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+const myLibrary = []
+
 function book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    book.call(this, title, author, pages, read);
+
     this.info = function (){
         return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
     }
+    myLibrary.push(this);
 }
 
-theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
+Object.setPrototypeOf(addBookToLibrary.prototype, book.prototype);
+//theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
 
-console.log(theHobbit.info());
+const hobbit = new addBookToLibrary('The Hobbit', 'Tolkien', 500, 'Not read');
